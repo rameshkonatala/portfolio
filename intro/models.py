6ddb40facaa63f841ce0606669af60ad061b63d4
rameshkonatala <konatalaramesh@gmail.com>
2016-11-27 19:56:26 +0530

    now contact form updates databases and saves client information

--- intro/models.py
+++ intro/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.utils import timezone
+from django.db import models
+from datetime import timedelta,datetime
+
+
+class Client(models.Model):
+	name=models.CharField(max_length=30)
+	mail_id=models.EmailField(max_length=30,primary_key=True)
+	comment=models.TextField()
+	def __str__(self):
+		return self.name
+	
--- intro/views.py
+++ intro/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from forms import ClientForm
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect
+from .models import Client
 
 # Create your views here.
 
 
 def index(request):
-    return render(request,'intro/index.html')
+	if request.POST:
+		form=ClientForm(request.POST)
+		if form.is_valid():
+			form.save()
+
+			return HttpResponseRedirect('/')
+	else:
+		form=ClientForm()
+	
+	return render(request,'intro/index.html',{'form':form})	
